9f15acea250b4be865bdd9c147fb64c99acd4a03
Ritabrata Das <dasritabrata5@gmail.com>
2022-12-16 02:14:10 +0530

    first commit

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+poetry install
+
+python3 manage.py collectstatic --no-input
+python3 manage.py makemigrations
+python3 manage.py migrate --run-syncdb
